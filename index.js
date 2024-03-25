984ef4bd15f0731d058ca54a2d8b040582bf6fa0
ajs <140010746+aboodJS@users.noreply.github.com>
2024-03-25 18:42:04 +0300

    added a signup form

--- index.js
+++ index.js
@@ -3,13 +3,18 @@ let signupBtn = document.querySelector(".signup");
 let logoutBtn = document.querySelector(".logout");
 let userNameField = document.querySelector("#username");
 let passWordField = document.querySelector("#password");
-let loginForm = document.querySelector("main form");
-let submitBtn = document.querySelector("form .submit");
-let closeForm = document.querySelector("form .close");
+let loginForm = document.querySelector(".login_form");
+let signupForm = document.querySelector(".signup_form");
+let submitForm = document.querySelectorAll("form .submit");
+let closeForm = document.querySelectorAll("form .close");
 
-localStorage.setItem("signin", false);
+if (localStorage.length == 0) {
+  localStorage.setItem("sign up", false);
+} else {
+  localStorage.setItem("sign up", true);
+}
 
-let account = localStorage.getItem("signin");
+let account = localStorage.getItem("sign up");
 
 logoutBtn.classList.add("hideBtn");
 
@@ -22,16 +27,24 @@ if (account === "false") {
 
 loginBtn.addEventListener("click", () => loginForm.classList.add("visiable"));
 
-closeForm.addEventListener("click", () =>
-  loginForm.classList.remove("visiable")
-);
+submitForm.forEach((elem) => {
+  elem.addEventListener("click", () => {
+    localStorage.setItem("username", userNameField.value);
+    localStorage.setItem("password", passWordField.value);
+  });
+});
 
-submitBtn.addEventListener("click", () => {
-  localStorage.setItem("userName", userNameField.value);
-  localStorage.setItem("password", passWordField.value);
+closeForm.forEach((elem) => {
+  elem.addEventListener("click", () => {
+    elem.parentElement.classList.remove("visiable");
+  });
 });
 
 logoutBtn.addEventListener("click", () => {
   localStorage.clear();
   location.reload();
+});
+
+signupBtn.addEventListener("click", () => {
+  signupForm.classList.add("visiable");
 });
